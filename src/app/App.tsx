8228fe64b40286bad9c97be06a6631dd5f96af5e
evdmatvey <evd.matvey@mail.ru>
2024-07-02 13:45:45 +0700

    Add auth check

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,25 @@
-function App() {
-  return <div>App</div>;
-}
+import { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Header } from '@/widgets/Header';
+import { selectFetchUser, useUserStore } from '@/entities/user';
+import { Layout } from '@/shared/ui/Layout';
+import Router from './router/Router';
+
+const App = () => {
+  const fetchUser = useUserStore(selectFetchUser);
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
+  return (
+    <>
+      <Layout headerSlot={<Header />}>
+        <Router />
+      </Layout>
+      <Toaster position="top-right" toastOptions={{ duration: 1500 }} />
+    </>
+  );
+};
 
 export default App;
