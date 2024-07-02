52e15a9664737dc0d90d5d050cf1d9da7b3a8044
evdmatvey <evd.matvey@mail.ru>
2024-07-02 17:42:36 +0700

    Add dashboard layout

---
+++ src/shared/ui/DashboardLayout/index.ts
@@ -0,0 +1,3 @@
+import DashboardLayout from './ui/DashboardLayout';
+
+export { DashboardLayout };
---
+++ src/shared/ui/DashboardLayout/ui/DashboardLayout.tsx
@@ -0,0 +1,45 @@
+import clsx from 'clsx';
+import { NavLink, Outlet } from 'react-router-dom';
+
+const DashboardLayout = () => {
+  const getLinkClasses = (isActive: boolean) => {
+    const classes = clsx('p-3 block rounded-lg transition-colors', {
+      'bg-red-100 hover:bg-red-100': isActive,
+      'bg-white hover:bg-red-50': !isActive,
+    });
+
+    return classes;
+  };
+
+  return (
+    <div className="flex items-start gap-x-10">
+      <aside className="w-[300px] bg-white p-5 rounded-xl shadow-sm grow-0 shrink-0 sticky top-10">
+        <nav>
+          <ul className="flex flex-col gap-3 max-h-[530px] overflow-y-auto pr-2">
+            <li>
+              <NavLink
+                className={({ isActive }) => getLinkClasses(isActive)}
+                to="/"
+              >
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => getLinkClasses(isActive)}
+                to="/categories"
+              >
+                Категории
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <div className="grow bg-white p-5 rounded-xl shrink">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
