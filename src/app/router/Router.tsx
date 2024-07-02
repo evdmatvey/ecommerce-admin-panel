a76a0d0c5f9e524d9e8c60a6aa6a5701ca2b41c5
evdmatvey <evd.matvey@mail.ru>
2024-07-02 17:43:59 +0700

    Add layout to routing

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,13 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Categories from '@/pages/Categories';
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
+import { DashboardLayout } from '@/shared/ui/DashboardLayout';
 import ProtectedRoute from './ProtectedRoute';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProtectedRoute children={<Home />} />} />
+        <Route
+          path="/"
+          element={<ProtectedRoute children={<DashboardLayout />} />}
+        >
+          <Route path="/" element={<Home />} />
+          <Route path="categories" element={<Categories />} />
+        </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
