45c613f2ee492f82936a55bf79bbf9a44f9bb46f
evdmatvey <evd.matvey@mail.ru>
2024-07-03 20:12:38 +0700

    Add react query provider

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,4 +1,15 @@
+import { CategoriesList } from '@/widgets/CategoriesList';
+import { CreateCategory } from '@/features/CreateCategory';
+
 const Categories = () => {
-  return <>Categories</>;
+  return (
+    <>
+      <h1 className="font-semibold mb-5">Категории</h1>
+      <div className="mb-5">
+        <CreateCategory />
+      </div>
+      <CategoriesList />
+    </>
+  );
 };
 export default Categories;
