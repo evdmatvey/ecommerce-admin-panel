1644f8eae2e3329992856c9439af8df5ee3da5e2
evdmatvey <evd.matvey@mail.ru>
2024-07-04 22:13:56 +0700

    Implement title only card ui component at categories

--- src/entities/category/ui/CategoryCard.tsx
+++ src/entities/category/ui/CategoryCard.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react';
+import { TitleOnlyCard } from '@/shared/ui/TitleOnlyCard';
 import { Category } from '../model/types';
 
 interface CategoryCardProps {
@@ -13,13 +14,11 @@ const CategoryCard: FC<CategoryCardProps> = ({
   updateButtonSlot,
 }) => {
   return (
-    <div className="bg-red-50 shadow-sm p-3 rounded-lg flex justify-between items-center">
-      {category.title}
-      <div className="flex items-center gap-2">
-        {updateButtonSlot}
-        {deleteButtonSlot}
-      </div>
-    </div>
+    <TitleOnlyCard
+      data={category}
+      deleteButtonSlot={deleteButtonSlot}
+      updateButtonSlot={updateButtonSlot}
+    />
   );
 };
 
